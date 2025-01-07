77a18d30a7915128a1abdaed8948e5fa56393862
inmyownwords7 <andrew.j.lansangan@gmail.com>
2025-01-07 14:33:55 -0500

    initialize software

---
+++ type.d.ts
@@ -0,0 +1,12 @@
+// forgot to add export {} requirements for it is export {} and declare global {}
+export {}
+declare global {
+
+    type userInput = {
+        initialInvestment: number;
+        annualInvestment: number;
+        expectedReturn: number;
+        duration: number;
+    }
+
+}
